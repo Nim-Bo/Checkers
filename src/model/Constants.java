18d0b92435032b3da2f8851c878620cdc602cb07
n.boghrati <n.boghrati@partsoftware.com>
2026-01-04 15:53:16 +0330

    implement constants

--- src/model/Constants.java
+++ src/model/Constants.java
@@ -1,4 +1,14 @@
 package model;
 
 public class Constants {
+    int BOARD_SIZE = 6;
+
+    int EMPTY = 0;
+    int BLACK = 1;
+    int WHITE = 2;
+    int BLACK_KING = 3;
+    int WHITE_KING = 4;
+
+    int BLACK_PLAYER = 1;
+    int WHITE_PLAYER = 2;
 }
