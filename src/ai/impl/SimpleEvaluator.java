8bc3beac1e2eaa2cf7b93635e3fb429dfa62cedf
Nim-Bo <Boghrat83@gmail.com>
2026-01-04 17:24:47 +0330

    implement SimpleEvaluator

--- src/ai/impl/SimpleEvaluator.java
+++ src/ai/impl/SimpleEvaluator.java
@@ -1,6 +1,27 @@
 package ai.impl;
 
 import ai.contracts.Evaluator;
+import model.Board;
+import model.Constants;
 
-public class SimpleEvaluator {
+public class SimpleEvaluator extends Constants implements Evaluator {
+
+    @Override
+    public int evaluate(Board board, int playerColor) {
+        int blackScore = 0;
+        int whiteScore = 0;
+
+        for (int row = 0; row < BOARD_SIZE; row++) {
+            for (int col = 0; col < BOARD_SIZE; col++) {
+                int piece = board.getPiece(row, col);
+
+                if (piece == BLACK) blackScore += 10;
+                else if (piece == BLACK_KING) blackScore += 15;
+                else if (piece == WHITE) whiteScore += 10;
+                else if (piece == WHITE_KING) whiteScore += 15;
+            }
+        }
+
+        return blackScore - whiteScore;
+    }
 }
