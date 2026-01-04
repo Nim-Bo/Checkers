4a09b64f86f8433a1bb655611318966859e4a8ce
n.boghrati <n.boghrati@partsoftware.com>
2026-01-04 16:06:17 +0330

    implement movement

--- src/model/Movement.java
+++ src/model/Movement.java
@@ -1,4 +1,27 @@
 package model;
 
 public class Movement {
+    private int fromRow, fromCol;
+    private int toRow, toCol;
+
+    public Movement(int fromRow, int fromCol, int toRow, int toCol) {
+        this.fromRow = fromRow;
+        this.fromCol = fromCol;
+        this.toRow = toRow;
+        this.toCol = toCol;
+    }
+
+    public boolean isJump() {
+        return Math.abs(fromRow - toRow) == 2;
+    }
+
+    public int getFromRow() { return fromRow; }
+    public int getFromCol() { return fromCol; }
+    public int getToRow() { return toRow; }
+    public int getToCol() { return toCol; }
+
+    @Override
+    public String toString() {
+        return "(" + fromRow + "," + fromCol + ") -> (" + toRow + "," + toCol + ")";
+    }
 }
