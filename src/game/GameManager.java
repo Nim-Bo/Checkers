6d9c375cbc3f72678096f7cea61a87c158c65b4e
Fatemeh Gharche <fatemehgharche8@gmail.com>
2026-01-08 00:43:51 +0330

    add GameManager & implement main game loop

--- src/game/GameManager.java
+++ src/game/GameManager.java
@@ -1,4 +1,44 @@
 package game;
 
-public class GameManager {
+import model.Board;
+import model.Constants;
+import model.Movement;
+import player.contracts.Player;
+import ui.Console;
+
+public class GameManager extends Constants {
+    private Board board;
+    private Player playerBlack;
+    private Player playerWhite;
+    private Console ui;
+
+    public GameManager(Board board, Player playerBlack, Player playerWhite, Console ui) {
+        this.board = board;
+        this.playerBlack = playerBlack;
+        this.playerWhite = playerWhite;
+        this.ui = ui;
+    }
+
+    public void startGame() {
+        Player currentPlayer = playerBlack;
+
+        while (true) {
+            ui.printBoard(board);
+
+            if (board.isGameOver(currentPlayer.getColor())) {
+                handleGameOver(currentPlayer);
+                break;
+            }
+
+            ui.showMessage("Turn: " + (currentPlayer.getColor() == BLACK ? "Black" : "White"));
+
+            Movement move = currentPlayer.makeMove(board);
+
+            board.makeMove(move);
+
+            ui.showMessage("Moved: " + move);
+
+            currentPlayer = (currentPlayer == playerBlack) ? playerWhite : playerBlack;
+        }
+    }
 }
