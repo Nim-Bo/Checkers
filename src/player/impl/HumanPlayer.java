705582356e896c8626fabed2b5495f12baa22f2c
Fatemeh Gharche <fatemehgharche8@gmail.com>
2026-01-08 01:18:43 +0330

    implement makeMove with validation and helper methods

--- src/player/impl/HumanPlayer.java
+++ src/player/impl/HumanPlayer.java
@@ -16,4 +16,43 @@ public class HumanPlayer implements Player {
         this.console = console;
     }
 
+    @Override
+    public Movement makeMove(Board board) {
+        List<Movement> legalMoves = board.getLegalMoves(color);
+
+        if (legalMoves.isEmpty()) {
+            return null;
+        }
+
+        while (true) {
+            console.showMessage("Your turn (" + (color == 1 ? "Black" : "White") + "). Available moves: " + legalMoves.size());
+            Movement move = console.getMoveInput();
+
+            if (move != null) {
+                for (Movement legal : legalMoves) {
+                    if (legal.getFromRow() == move.getFromRow() &&
+                            legal.getFromCol() == move.getFromCol() &&
+                            legal.getToRow() == move.getToRow() &&
+                            legal.getToCol() == move.getToCol()) {
+                        return legal;
+                    }
+                }
+            }
+
+            console.showMessage("Invalid move! You must choose from legal moves (mandatory jumps applied).");
+            for(Movement m : legalMoves) {
+                System.out.println("Hint: " + m);
+            }
+        }
+    }
+
+    @Override
+    public int getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean isHuman() {
+        return true;
+    }
 }
