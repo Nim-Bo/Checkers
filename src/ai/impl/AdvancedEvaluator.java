bfa43edd2187c024d836fc034a8253ae3fab2e88
Fatemeh Gharche <fatemehgharche8@gmail.com>
2026-01-07 20:21:28 +0330

    add AdvancedEvaluator & iterate board and prepare score aggregation

--- src/ai/impl/AdvancedEvaluator.java
+++ src/ai/impl/AdvancedEvaluator.java
@@ -1,4 +1,25 @@
 package ai.impl;
 
-public class AdvancedEvaluator {
+import ai.contracts.Evaluator;
+import model.Board;
+import model.Constants;
+
+public class AdvancedEvaluator extends Constants implements Evaluator {
+
+    @Override
+    public int evaluate(Board board, int playerColor) {
+        int blackScore = 0;
+        int whiteScore = 0;
+
+        for (int row = 0; row < BOARD_SIZE; row++) {
+            for (int col = 0; col < BOARD_SIZE; col++) {
+                int piece = board.getPiece(row, col);
+                if (piece == EMPTY) continue;
+
+
+            }
+        }
+
+        return blackScore - whiteScore;
+    }
 }
