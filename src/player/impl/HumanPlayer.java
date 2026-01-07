2aa230ecb99bf1015b6f000c3ecbf93e849467d0
Fatemeh Gharche <fatemehgharche8@gmail.com>
2026-01-08 01:14:09 +0330

    add HumanPlayer skeleton and constructor

--- src/player/impl/HumanPlayer.java
+++ src/player/impl/HumanPlayer.java
@@ -1,4 +1,19 @@
 package player.impl;
 
-public class HumanPlayer {
+import model.Board;
+import model.Movement;
+import player.contracts.Player;
+import ui.Console;
+
+import java.util.List;
+
+public class HumanPlayer implements Player {
+    private int color;
+    private Console console;
+
+    public HumanPlayer(int color, Console console) {
+        this.color = color;
+        this.console = console;
+    }
+
 }
